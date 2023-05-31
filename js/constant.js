f7498002f8964e4d10f01e7d7a3a6d6a43afb1a3
hanyanhsiao <j0955551237@gmail.com>
2023-05-31 13:17:22 +0800

    修正上伺服器發現的問題，有更改到header.html

--- js/constant.js
+++ js/constant.js
@@ -3,10 +3,9 @@
 // ==========勿動勿動勿動勿動勿動勿動勿動勿動===========
 
 // 本地端伺服器
-    
 const php_url = "http://localhost/brainstem_php/";
 
 // 緯育伺服器
 // const php_url = "https://tibamef2e.com/thd101/g2/php/";
-    
+
 // ==========勿動勿動勿動勿動勿動勿動勿動勿動===========
